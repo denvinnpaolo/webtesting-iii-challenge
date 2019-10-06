dcadef3b8d65a1105f69ec442b9cf30bd0b9cf81
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2019-10-05 17:45:06 -0700

    first tests finsihed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+//Library imports
+
+import Dashboard from "./Dashboard";
+
+
+test("Look for the Display and Control Components", () => {
+    const { findByTitle } = render(<Dashboard />);
+
+    const displayComponent = findByTitle(/Display/i)
+    const controlComponent = findByTitle(/Controls/i)
+});
